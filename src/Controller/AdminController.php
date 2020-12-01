4d5c04511d776ee2975408d94a01cd6cfc7e1be0
Asmodee7 <matthieu.sivy@gmail.com>
2020-12-01 17:20:17 +0100

    dev Inscription + connexion + role user + debut back office

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/articles", name="admin_articles")
+     */
+    public function adminArticles(): Response
+    {
+        return $this->render('admin/admin_articles.html.twig');
+    }
+}   
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,27 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ *      fields = {"email"},
+ *      message="Un compte est déjà existant à cette adresse Email !"
+ * )
+ * @UniqueEntity(
+ *      fields ={"username"},
+ *      message="Pseudo déjà utiliser, Veuillez en saisir un autre !"
+ * )
  */
-class User
+
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -19,20 +33,48 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez renseigner un Email !")
+     * @Assert\Email(message="Veuillez renseigner un Email Valide ! ex : Foo@gmail.com")
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez saisir un nom d'utilisateur !")
+     * @Assert\Length(
+     *      min="2",
+     *      minMessage="Votre nom d'utilisateur doit contenir minimum 2 caractères !"
+     * )
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(                       
+     *        min="8",
+     *        minMessage="Votre mot de passe doit faire minimum 8 caractères !"
+     * )
+     * @Assert\EqualTo(
+     *      propertyPath="confirm_password",
+     *      message="Les mot de passe ne correspondent pas ! Vérifiez la saisie."
+     * )
      */
     private $password;
 
+    /**
+     * @Assert\EqualTo(
+     *      propertyPath="password",
+     *      message="Les mot de passe ne correspondent pas ! Vérifiez la saisie."
+     * )
+     */
     public $confirm_password;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+
 
     public function getId(): ?int
     {
@@ -74,4 +116,30 @@ class User
 
         return $this;
     }
+
+    // Cette méthode est uniquement destinée a nettoyer les mots de passe en texte brute éventuellement stockés
+    public function eraseCredentials(){
+
+    }
+
+    // Renvoie la chaine de caractère non encodé que l'utilisateur a saisi, qui est utilisée à l'origine pour encodé le mot de passe
+    public function getSalt(){
+
+    }
+
+    // Cette fonction renvoi un tableau de chaine de caractères
+    // Renvoie les roles accordés à l'utilisateur.
+    public function getRoles(){
+        return $this->roles;
+
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    
 }
